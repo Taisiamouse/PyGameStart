3e04814667508296f119a12e2d98657945069b02
Taisia <tasyabordun@gmail.com>
2020-10-04 12:17:19 +0300

    add random function

--- lesson4/tictactoe.py
+++ lesson4/tictactoe.py
@@ -8,6 +8,7 @@
 6. play again?
 
 """
+import random
 
 def inputPlayerLetter():
     letter = ''
@@ -20,6 +21,20 @@ def inputPlayerLetter():
         return ['O', 'X']  # [player, bot]
 
 #print(inputPlayerLetter()) # check!
+
+def  choiceRandom():
+    if random.randint(0, 1)== 0:
+        return ['X', 'O'] #'Bot'
+    else:
+        return ['O', 'X'] #'Player'
+
+def choiceX():
+    return['X', 'O']
+
+def choiceO():
+    return['O', 'X']
+
+
 def printWelcome():
     menu = """
     Menu:
@@ -33,4 +48,19 @@ def printWelcome():
     while not (choice == 1 or choice == 2 or choice == 3 or choice == 4): 
         choice = int(input('')) # str -> int
 
+
+    if (choice == 1):
+        return choiceX()
+    elif (choice == 2):
+        return choiceO()
+    elif (choice == 3):
+        return choiceRandom()
+    elif (choice == 4):
+        return inputPlayerLetter() 
+    else:
+        return -1 # ERROR      
+
+choices = printWelcome()
+print('Player goes', choices[0])
+print('Bot goes', choices[1]) 
                
